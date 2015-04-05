030607d44bb1e1dc5a7df2c75c057dedb40a945a
Albert Gareev <whosuchalbert@yandex.ru>
2015-04-05 17:07:31 +0300

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  
+ 	has_many :comments
+ 	has_many :articles
+
+  devise :database_authenticatable,
+  		 :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable, 
+         :validatable
+end
